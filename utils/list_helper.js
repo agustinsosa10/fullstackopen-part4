baf4b759a4cbc541777b0b11f680954672d47c9b
agustinsosa10 <118317303+agustinsosa10@users.noreply.github.com>
2025-12-21 15:01:18 -0300

    ejercicio 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+
+    return blogs.length === 0 ? 0 : blogs.reduce( (actual, blogs) => actual + blogs.likes, 0)
+    
+}
+
 module.exports = {
-    dummy
+    dummy, totalLikes
 }
