fd7857acd549237a2181ccedd9e130540a389eec
agustinsosa10 <118317303+agustinsosa10@users.noreply.github.com>
2025-12-22 20:20:52 -0300

    ejercicio 4.7

--- tests/prueba.test.js
+++ tests/prueba.test.js
@@ -1,16 +1,18 @@
-const { test, describe } = require('node:test')
-const assert = require('node:assert')
-const listHelper = require('../utils/list_helper')
+const { test, describe } = require("node:test");
+const assert = require("node:assert");
+const listHelper = require("../utils/list_helper");
+const _ = require("lodash");
 
-const emptyBlog = []
-    const blogs = [
+const emptyBlog = [];
+
+const blogs = [
   {
     _id: "5a422a851b54a676234d17f7",
     title: "React patterns",
     author: "Michael Chan",
     url: "https://reactpatterns.com/",
     likes: 7,
-    __v: 0
+    __v: 0,
   },
   {
     _id: "5a422aa71b54a676234d17f8",
@@ -18,7 +20,7 @@ const emptyBlog = []
     author: "Edsger W. Dijkstra",
     url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
     likes: 5,
-    __v: 0
+    __v: 0,
   },
   {
     _id: "5a422b3a1b54a676234d17f9",
@@ -26,7 +28,7 @@ const emptyBlog = []
     author: "Edsger W. Dijkstra",
     url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
     likes: 12,
-    __v: 0
+    __v: 0,
   },
   {
     _id: "5a422b891b54a676234d17fa",
@@ -34,7 +36,7 @@ const emptyBlog = []
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
     likes: 10,
-    __v: 0
+    __v: 0,
   },
   {
     _id: "5a422ba71b54a676234d17fb",
@@ -42,7 +44,7 @@ const emptyBlog = []
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
     likes: 0,
-    __v: 0
+    __v: 0,
   },
   {
     _id: "5a422bc61b54a676234d17fc",
@@ -50,73 +52,132 @@ const emptyBlog = []
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
     likes: 14,
-    __v: 0
-  } 
-]
+    __v: 0,
+  },
+];
 
-const singleBlog = [ 
-      {
+const singleBlog = [
+  {
     _id: "5a422bc61b54a676234d17fc",
     title: "Type wars",
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
     likes: 2,
-    __v: 0
-  } 
-]
+    __v: 0,
+  },
+];
 
-test('dummy returns one', () => {
-    const blogs = []
+test("dummy returns one", () => {
+  const blogs = [];
 
-    const result = listHelper.dummy(blogs)
-    assert.strictEqual(result, 1)
-})
+  const result = listHelper.dummy(blogs);
+  assert.strictEqual(result, 1);
+});
 
-describe('total likes', () => {
+describe("total likes", () => {
+  test("una lista vacia es 0", () => {
+    const result = listHelper.totalLikes(emptyBlog);
+    assert.strictEqual(result, 0);
+  });
 
-test('una lista vacia es 0', () => {
+  test("cuando una lista tiene un solo blog el total deben ser los likes de ese blog", () => {
+    const result = listHelper.totalLikes(singleBlog);
+    assert.strictEqual(result, singleBlog[0].likes);
+  });
 
-    const result = listHelper.totalLikes(emptyBlog)
-    assert.strictEqual(result, 0)
-})
+  test("el total de likes de la lista completa es calculado correctamente", () => {
+    const result = listHelper.totalLikes(blogs);
+    const total = blogs.reduce((acc, blog) => {
+      return acc + blog.likes;
+    }, 0);
+    assert.strictEqual(result, total);
+  });
+});
 
-test('cuando una lista tiene un solo blog el total deben ser los likes de ese blog', () => {
-    const result = listHelper.totalLikes(singleBlog)
-    assert.strictEqual(result, singleBlog[0].likes)
-})
+describe("blog con mas me gustas", () => {
+  test("una lista vacia no devuelve nada", () => {
+    const result = listHelper.favoriteBlog(emptyBlog);
+    assert.deepStrictEqual(result, null);
+  });
 
-test('el total de likes de la lista completa es calculado correctamente', () => {
-    const result = listHelper.totalLikes(blogs)
-    const total = blogs.reduce( (acc, blog) =>  { return acc + blog.likes}, 0)
-    assert.strictEqual(result, total)
-})
+  test("una lista con un solo blog, devuelve el mismo", () => {
+    console.log(singleBlog);
+    console.log(typeof singleBlog);
+    const result = listHelper.favoriteBlog(singleBlog);
+    console.log("desde test viendo el blog con una sola entrada ", result);
 
-})
+    assert.deepStrictEqual(result, singleBlog[0]);
+  });
 
-describe ('blog con mas me gustas', () => {
-    
-  test('una lista vacia no devuelve nada', () => {
-    const result = listHelper.favoriteBlog(emptyBlog)
-    assert.deepStrictEqual(result, null)
-  })
-
-  test('una lista con un solo blog, devuelve el mismo', () => {
-    const result = listHelper.favoriteBlog(singleBlog)
-    
-    assert.deepStrictEqual(result, singleBlog([0]))
-  })
-
-  test('de una lista se devuelve el blog con mas likes', () => {
-
-    const result = listHelper.favoriteBlog(blogs)
+  test("de una lista se devuelve el blog con mas likes", () => {
+    const result = listHelper.favoriteBlog(blogs);
 
     const blogWithMoreLikes = blogs.reduce((max, blog) => {
-      return blog.likes > max.likes ? blog : max
-    }, blogs[0])
+      return blog.likes > max.likes ? blog : max;
+    }, blogs[0]);
 
-    console.log('desde prueba 2')
-    console.log('blog con mas likes',blogWithMoreLikes)
+    console.log("desde prueba 2");
+    console.log("blog con mas likes", blogWithMoreLikes);
 
-    assert.deepStrictEqual(result, blogWithMoreLikes)
-  })
-})
+    assert.deepStrictEqual(result, blogWithMoreLikes);
+  });
+});
+
+describe("autores con mas blogs", () => {
+  test("una lista vacia no devuelve ningun autor", () => {
+    const result = listHelper.mostBlogs(emptyBlog);
+    assert.deepStrictEqual(result, null);
+  });
+
+  test("una lista con un solo blog devuelve el autor del mismo", () => {
+    const { author } = singleBlog[0];
+    console.log("desde test, el autor es:", author);
+    const result = listHelper.mostBlogs(singleBlog);
+    assert.strictEqual(result, author);
+  });
+
+  test("de la lista completa devuelve el autor con mas blog", () => {
+    const counts = _.countBy(blogs, "author");
+
+    //entre todos los autores que aparecen en la lista de blog, se queda con el que tiene mas blogs
+    const author = _.maxBy(
+      Object.keys(counts), //al objeto counts (autores), se los transforma en clave
+      (autor) => counts[autor] //aca se obtiene el valor de cada clave guardada en el objeto counts(osea cuantas veces aparece cada autor en la lista de blogs)
+    );
+
+    const result = listHelper.mostBlogs(blogs);
+    assert.strictEqual(result, author);
+  });
+});
+
+describe("autor con mas likes", () => {
+  test("un lista vacia no devuelve nada", () => {
+    const result = listHelper.mostLikes(emptyBlog);
+    assert.strictEqual(result, null);
+  });
+
+  test("una lista con un solo blog devuelve los likes de ese blog y el nombre del autor", () => {
+    const { author, likes } = singleBlog[0];
+    const authorWithMoreLikes = { author, likes };
+    console.log(authorWithMoreLikes);
+    const result = listHelper.mostLikes(singleBlog);
+    assert.deepStrictEqual(result, authorWithMoreLikes);
+  });
+
+  test("de una lista se devuelve el autor con mas likes y la cantidad de likes", () => {
+    const grouped = _.groupBy(blogs, "author");
+
+    //autor que contiene mas likes
+    const author = _.maxBy(
+      Object.keys(grouped), //al objeto counts (autores), se los transforma en clave
+      (autor) => _.sumBy(grouped[autor], "likes") //se suma la cantidad de likes de cada autor
+    );
+
+    const likes = _.sumBy(grouped[author], "likes");
+
+    const authorWithMoreLikes = { author, likes };
+    console.log('autor con mas likes de una lista', authorWithMoreLikes)
+    const result = listHelper.mostLikes(blogs);
+    assert.deepStrictEqual(result, authorWithMoreLikes);
+  });
+});
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,24 +1,66 @@
+const _ = require("lodash");
+
 const dummy = (blogs) => {
-    return 1
-}
+  return 1;
+};
 
 const totalLikes = (blogs) => {
-
-    return blogs.length === 0 ? 0 : blogs.reduce( (actual, blogs) => actual + blogs.likes, 0)
-    
-}
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce((actual, blogs) => actual + blogs.likes, 0);
+};
 
 const favoriteBlog = (blogs) => {
+  // console.log('desde list helper',blogs)
+  if (blogs.length === 0) {
+    return null;
+  }
 
-    if ( blogs.length === 0) {return null}
+  return blogs.reduce((max, blogActual) => {
+    // console.log('valor maximo actual:', max)
+    // console.log('blog actual:', blogActual)
+    return blogActual.likes > max.likes ? blogActual : max;
+  }, blogs[0]);
+};
 
-    return blogs.reduce( (max, blogActual) => {
-        console.log('valor maximo actual:', max)
-        console.log('blog actual:', blogActual)
-        return blogActual.likes > max.likes ? blogActual : max
-    }, blogs[0])
-}
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null;
+
+  //cuenta cuantas veces aparece cada autor en la lista de blogs
+  const counts = _.countBy(blogs, "author");
+
+  //entre todos los autores que aparecen en la lista de blog, se queda con el que tiene mas blogs
+  const author = _.maxBy(
+    Object.keys(counts), //al objeto counts (autores), se los transforma en clave
+    (autor) => counts[autor] //aca se obtiene el valor de cada clave guardada en el objeto counts(osea cuantas veces aparece cada autor en la lista de blogs)
+  );
+
+  console.log("desde la funcion mostblogs", author);
+  return author;
+};
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) return null;
+
+  //agrupa los autores con sus blogs 
+  const grouped = _.groupBy(blogs, "author");
+
+  //autor que contiene mas likes
+  const author = _.maxBy(
+    Object.keys(grouped), //al objeto counts (autores), se los transforma en clave
+    (autor) => _.sumBy(grouped[autor], 'likes') //se suma la cantidad de likes de cada autor
+  );
+  
+  return {
+    author,
+    likes: _.sumBy(grouped[author], 'likes')
+  }
+};
 
 module.exports = {
-    dummy, totalLikes, favoriteBlog
-}
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
+};
