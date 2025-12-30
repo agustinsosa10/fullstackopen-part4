0590ca8ff1018f5b79fa95eeb6610530e01eb064
agustinsosa10 <118317303+agustinsosa10@users.noreply.github.com>
2025-12-29 21:09:49 -0300

    cambiando promesas por async/await - ejercicio 4.8

---
+++ tests/bloglist_api.test.js
@@ -0,0 +1,39 @@
+const { test, after, beforeEach } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+const app = require('../app')
+const assert = require('node:assert')
+
+const api = supertest(app)
+//esta funcion es para que cada vez que hagamos una prueba la base de datos de prueba este igual en todos los casos (2 blogs)
+beforeEach( async () => {
+    await Blog.deleteMany({})
+
+    for (const blog of helper.initialBlogs) {
+        let blogObject = new Blog(blog)
+        await blogObject.save()
+    }
+})
+
+test('los blogs son devueltos en json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
+
+test('devuelve la cantidad de notas correctas', async () => {
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+    
+})
+
+after(async () => {
+    await mongoose.connection.close()
+})
+
+
+
---
+++ tests/test_helper.js
@@ -0,0 +1,16 @@
+const initialBlogs = [
+  {
+    title: 'Primer blog',
+    author: 'Agustin',
+    url: 'http://example.com',
+    likes: 5,
+  },
+  {
+    title: 'Segundo blog',
+    author: 'Chelo',
+    url: 'http://example.com/2',
+    likes: 10,
+  }
+]
+
+module.exports = { initialBlogs }
